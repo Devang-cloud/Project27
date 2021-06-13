d24a52514ceeaeb201e4b7c15ea21c23a7a80a56
Devang-cloud <73704753+Devang-cloud@users.noreply.github.com>
2021-06-13 11:03:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+function preload(){
+
+	
+}
+
+function setup() {
+	createCanvas(600, 500);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	var bobDiameter = 40;
+
+	//Create the Bodies Here.
+	roof = new Roof(300, 100, 300, 20);
+
+	bobObject1 = new Bob(200, 400, 50);
+	bobObject2 = new Bob(250, 400, 50);
+	bobObject3 = new Bob(300, 400, 50);
+	bobObject4 = new Bob(350, 400, 50);
+	bobObject5 = new Bob(400, 400, 50);
+
+	rope1 = new Rope(bobObject1.body,roof.body,-bobDiameter*2.4,0);
+	rope2 = new Rope(bobObject2.body,roof.body,-bobDiameter*1.3,0);
+	rope3 = new Rope(bobObject3.body,roof.body,0,0); 
+ 	rope4 = new Rope(bobObject4.body,roof.body,bobDiameter*1.3,0); 
+	rope5 = new Rope(bobObject5.body,roof.body,bobDiameter*2.4,0);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+  
+  roof.display();
+  
+  drawSprites();
+ 
+}
+
+function keyPressed() { 
+	if (keyCode === UP_ARROW) 
+	{ Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-50}); } 
+} 
+
+
